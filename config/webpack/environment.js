ba3a1cd296f969e0f76dde52d1fe9be5acf70884
Vinchenso <chensok@gmail.com>
2017-12-16 17:50:44 +0200

    config linters, navbar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+module.exports = environment;
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
